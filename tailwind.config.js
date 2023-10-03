17cdaaad59542a6a242f7e8e7a3d042f43681e4c
Grenish rai <ariestheracer@gmail.com>
2023-10-03 23:09:51 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryFont: "#bfc7d2",
+        blackPrimary: "#19262C"
+      },
+      fontFamily: {
+        pop: ["Poppins", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
