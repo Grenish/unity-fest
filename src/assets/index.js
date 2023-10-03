48fc8efbb841829e1b78285a714be88d0a3511b2
Grenish rai <ariestheracer@gmail.com>
2023-10-03 23:09:26 +0530

    added assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import esports from './esports.jpeg';
+import coding from './coding.jpg';
+import cultural from './cultural.jpg';
+import sports from './sports.jpg';
+import photography from './photography.jpeg';
+import art from './art.jpg';
+
+export { esports, coding, cultural, sports, photography, art}
