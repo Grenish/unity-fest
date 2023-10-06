12bdee3de4ceb417123f5ac10783092e042ed4ef
Grenish rai <ariestheracer@gmail.com>
2023-10-06 23:41:13 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,7 @@ import Events from "./Events";
 import Hero from "./Hero";
 import MatrixRain from "./MatrixRain";
 import EventCards from "./EventCards";
+import About from "./About";
+import Sponsores from "./Sponsores";
 
-export { Navbar, Footer, Events, Hero, MatrixRain, EventCards };
+export { Navbar, Footer, Events, Hero, MatrixRain, EventCards, About, Sponsores };
