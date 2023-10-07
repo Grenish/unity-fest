5bfb42bca170fef9f6c10526784ef71c4f217c69
Grenish rai <ariestheracer@gmail.com>
2023-10-07 22:56:53 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ import cultural from './cultural.jpg';
 import sports from './sports.jpg';
 import photography from './photography.jpeg';
 import art from './art.jpg';
+import vite from './vite.svg';
 
-export { esports, coding, cultural, sports, photography, art}
+export { esports, coding, cultural, sports, photography, art, vite}
