871f75154708c87caeb64b00f77fba300ac4384e
Grenish rai <ariestheracer@gmail.com>
2023-10-07 22:58:21 +0530

    added placeholder sponsors images

--- src/assets/sponsores/index.js
+++ src/assets/sponsores/index.js
@@ -1,4 +1,9 @@
 import dominos from "./dominos.svg";
 import royalEnfield from "./royalEnfield.svg";
+import gcp from "./gcp.svg";
+import ibm from "./ibm.svg";
+import cocacola from "./cocacola.svg";
+import microsoft from "./microsoft.svg";
+import tesla from "./tesla.svg";
 
-export { dominos, royalEnfield };
+export { dominos, royalEnfield, gcp, ibm, cocacola, microsoft, tesla };
