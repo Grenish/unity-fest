e0629879f6f05186ddb476618b20bc6a391363be
Grenish rai <ariestheracer@gmail.com>
2023-10-14 00:54:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,17 @@ import {
   Hero,
   Navbar,
   Sponsors,
-  Discussion,
-  DiscussButton,
 } from "./components";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/discussion" element={<Discussion />} />
-        <Route path="/*" element={
-          <>
-            <Hero />
-            <About />
-            <EventCards />
-            <Sponsors />
-            <DiscussButton />
-            <Footer />
-          </>
-        } />
-      </Routes>
+      <Hero />
+      <About />
+      <EventCards />
+      <Sponsors />
+      <Footer />
     </div>
   );
 };
